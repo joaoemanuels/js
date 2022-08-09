08c744668c738bf2ab727bea8e5042baabaeaef4
Joao Emanuel <jemanuel.pi@gmail.com>
2022-08-09 15:57:15 -0300

    js

---
+++ script.js
@@ -0,0 +1,16 @@
+var parametro = prompt('digite um numero')
+
+switch (parseInt(parametro)) {
+
+    case 1:
+        document.write('parametro 1')
+        break
+
+    case 2:
+        document.write('parametro 2')
+        
+
+    default:
+        document.write('default')
+
+}
